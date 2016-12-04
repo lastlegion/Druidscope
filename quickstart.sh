38de2b5843c53aa6a0b105eb7540f423a825dbc2
Ganesh Iyer <lastlegion@gmail.com>
2016-12-04 16:34:58 -0500

    added quickstart.sh

---
+++ quickstart.sh
@@ -0,0 +1,15 @@
+echo "Starting historical"
+java `cat conf-quickstart/druid/historical/jvm.config | xargs` -cp "conf-quickstart/druid/_common:conf-quickstart/druid/historical:lib/*" io.druid.cli.Main server historical > historical.log &
+sleep 2
+echo "Starting broker"
+java `cat conf-quickstart/druid/broker/jvm.config | xargs` -cp "conf-quickstart/druid/_common:conf-quickstart/druid/broker:lib/*" io.druid.cli.Main server broker > broker.log &
+sleep 2
+echo "Starting coordinator"
+java `cat conf-quickstart/druid/coordinator/jvm.config | xargs` -cp "conf-quickstart/druid/_common:conf-quickstart/druid/coordinator:lib/*" io.druid.cli.Main server coordinator > coordinator.log &
+sleep 2
+echo "Starting overlord"
+java `cat conf-quickstart/druid/overlord/jvm.config | xargs` -cp "conf-quickstart/druid/_common:conf-quickstart/druid/overlord:lib/*" io.druid.cli.Main server overlord > overlord.log &
+sleep 2
+echo "Starting middle manager"
+java `cat conf-quickstart/druid/middleManager/jvm.config | xargs` -cp "conf-quickstart/druid/_common:conf-quickstart/druid/middleManager:lib/*" io.druid.cli.Main server middleManager > middleManager.log &
+
